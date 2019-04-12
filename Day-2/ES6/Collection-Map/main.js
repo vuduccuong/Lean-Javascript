c19f94ebc51d7109c56cd660c20aa3a55368506c
Cuongvd <vuduccuong.ck48@gmail.com>
2019-04-12 13:54:16 +0700

    Collection Map

---
+++ Day-2/ES6/Collection-Map/main.js
@@ -0,0 +1,13 @@
+let mapName = new Map([
+    ['name', 'Cường'],
+    ['age',23],
+    ['address','Ninh Bình']
+]);
+
+
+console.log(mapName);
+
+mapName.set('isBoy', true);
+console.log(mapName);
+
+mapName.delete();
